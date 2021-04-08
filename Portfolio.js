9c5c7f6cda7e7bba19a18ebee307ebee6b34499b
Manon <81436521+manonblt@users.noreply.github.com>
2021-04-08 19:49:38 +0200

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,153 @@
+TweenMax.staggerFromTo('h1 span', 0.1, {
+    autoAlpha: 0
+}, {
+    autoAlpha: 1
+}, 0.11)
+
+
+var g = new JustGage({
+    id: "gestion-projet",
+    customSectors: [{
+        color : "#ea9999",
+        lo : 0,
+        hi : 34
+    },{
+        color : "#f9cb9c",
+        lo : 34,
+        hi : 67
+    },{
+        color : "#b6d7a8",
+        lo : 67,
+        hi : 100
+    }],
+    donut: true,
+    valueFontFamily: 'Poppins',
+    valueFontColor: "#F2F2F2",
+    labelFontColor: "#F2F2F2",
+    titleFontColor: "#F2F2F2",
+    value: 80,
+    min: 0,
+    max: 100,
+    title: "Gestion de projets" 
+});
+var g = new JustGage({
+    id: "creativite",
+    customSectors: [{
+        color : "#ea9999",
+        lo : 0,
+        hi : 34
+    },{
+        color : "#f9cb9c",
+        lo : 34,
+        hi : 67
+    },{
+        color : "#b6d7a8",
+        lo : 67,
+        hi : 100
+    }],
+    donut: true,
+    valueFontFamily: 'Poppins',
+    valueFontColor: "#F2F2F2",
+    titleFontColor: "#F2F2F2",
+    value: 75,
+    min: 0,
+    max: 100,
+    title: "Créativité"
+});
+var g = new JustGage({
+    id: "rigueur",
+    customSectors: [{
+        color : "#ea9999",
+        lo : 0,
+        hi : 34
+    },{
+        color : "#f9cb9c",
+        lo : 34,
+        hi : 67
+    },{
+        color : "#b6d7a8",
+        lo : 67,
+        hi : 100
+    }],
+    donut: true,
+    valueFontFamily: 'Poppins',
+    valueFontColor: "#F2F2F2",
+    titleFontColor: "#F2F2F2",
+    value: 90,
+    min: 0,
+    max: 100,
+    title: "Rigueur"
+});
+var g = new JustGage({
+    id: "motivation",
+    customSectors: [{
+        color : "#ea9999",
+        lo : 0,
+        hi : 34
+    },{
+        color : "#f9cb9c",
+        lo : 34,
+        hi : 67
+    },{
+        color : "#b6d7a8",
+        lo : 67,
+        hi : 100
+    }],
+    donut: true,
+    valueFontFamily: 'Poppins',
+    valueFontColor: "#F2F2F2",
+    titleFontColor: "#F2F2F2",
+    value: 100,
+    min: 0,
+    max: 100,
+    title: "Motivation"
+});
+var g = new JustGage({
+    id: "resolution",
+    customSectors: [{
+        color : "#ea9999",
+        lo : 0,
+        hi : 34
+    },{
+        color : "#f9cb9c",
+        lo : 34,
+        hi : 67
+    },{
+        color : "#b6d7a8",
+        lo : 67,
+        hi : 100
+    }],
+    donut: true,
+    valueFontFamily: 'Poppins',
+    valueFontColor: "#F2F2F2",
+    titleFontColor: "#F2F2F2",
+    value: 70,
+    min: 0,
+    max: 100,
+    title: "Résolution de problèmes"
+});
+var g = new JustGage({
+    id: "communication",
+    customSectors: [{
+        color : "#ea9999",
+        lo : 0,
+        hi : 34
+    },{
+        color : "#f9cb9c",
+        lo : 34,
+        hi : 67
+    },{
+        color : "#b6d7a8",
+        lo : 67,
+        hi : 100
+    }],
+    donut: true,
+    valueFontFamily: 'Poppins',
+    valueFontColor: "#F2F2F2",
+    titleFontColor: "#F2F2F2",
+    value: 85,
+    min: 0,
+    max: 100,
+    title: "Communication"
+});
+
